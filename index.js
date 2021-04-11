5ffdddb3275698cc90babd0eef962115bd963da4
Clemente <dev@imkreative.com>
2021-04-11 04:08:05 -0400

    Add batch method

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
     vow: require('./src/vow'),
     wait: require('./src/helpers').wait,
-    reduce: require('./src/loops').reduce,
-    map: require('./src/loops').map
+    reduce: require('./src/collections').reduce,
+    map: require('./src/collections').map,
+    batch: require('./src/collections').batch
 }
